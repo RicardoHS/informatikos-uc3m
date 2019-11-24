39d96035c4445b0b11b29bebbde708f3e4d64c73
Santiago Raposo <santiraposo96@gmail.com>
2019-11-24 16:48:54 +0100

    Boxplot of variables

--- multivariate/main.R
+++ multivariate/main.R
@@ -2,3 +2,17 @@ library(tidyverse)
 
 # First column is just a pandas index.
 poke <- read_csv2("data/pokemon.csv")[,-1]
+
+poke_long <- poke %>%
+  select(-target, -contains("Type")) %>%
+  pivot_longer(cols = everything(),
+               names_to = c("Pokemon", "statistic"),
+               values_to = "value",
+               names_pattern = "poke(.)_(.*)")
+
+poke_long %>%
+  filter(!(statistic %in% c("Legendary", "Generation"))) %>%
+  ggplot()+
+  geom_boxplot(aes(x = statistic, y = value))+
+  coord_flip()
+
