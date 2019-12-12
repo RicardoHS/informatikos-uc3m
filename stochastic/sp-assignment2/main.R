5b271f968a2a57a8ed0ad2e4784f4f1525f461e0
javiermzll <javiermzl@outlook.com>
2019-12-12 18:52:47 +0100

    Descriptive Analysis

--- stochastic/sp-assignment2/main.R
+++ stochastic/sp-assignment2/main.R
@@ -15,3 +15,20 @@ retweet_data <- readRDS("data/retweet_data.rds")
 
 retweet_times <- map(retweet_data, "created_at")
 n_retweets <- map_dbl(retweet_times, length)
+
+
+# Descriptive Analysis
+which(n_retweets==max(n_retweets))
+
+times <- numeric(length(retweet_times[[54]]))
+times[1] <- difftime(retweet_times[[54]][1], tweets[54,]$created_at, units = "secs")[[1]]
+
+for (i in 1:(length(retweet_times[[54]]) - 1)){
+  times[i+1] <- difftime(retweet_times[[54]][i], retweet_times[[54]][i+1], units = "secs")[[1]]
+}
+
+rts <- as.data.frame(cbind(c(1:length(retweet_times[[54]])), times, cumsum(times)))
+colnames(rts) <- c("Count", "ms", "TotalTime")
+
+
+ggplot(rts) + geom_line() + aes(x=TotalTime, y=Count)
