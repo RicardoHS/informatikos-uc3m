64465fb47400b8cb59d734cb8f6c4ebd51c492d9
javiermzll <javiermzl@outlook.com>
2019-12-15 15:57:17 +0100

    Fix Inverted Times

--- stochastic/sp-assignment2/main.R
+++ stochastic/sp-assignment2/main.R
@@ -21,13 +21,14 @@ n_retweets <- map_dbl(retweet_times, length)
 which(n_retweets==max(n_retweets))
 
 times <- numeric(length(retweet_times[[54]]))
-times[1] <- difftime(retweet_times[[54]][1], tweets[54,]$created_at, units = "secs")[[1]]
+retweets <- rev(retweet_times[[54]])
+times[1] <- difftime(tweets[54,]$created_at, retweets[1], units = "min")[[1]]
 
-for (i in 1:(length(retweet_times[[54]]) - 1)){
-  times[i+1] <- difftime(retweet_times[[54]][i], retweet_times[[54]][i+1], units = "secs")[[1]]
+for (i in 1:(length(retweets) - 1)){
+  times[i+1] <- difftime(retweets[i+1], retweets[i], units = "min")[[1]]
 }
 
-rts <- as.data.frame(cbind(c(1:length(retweet_times[[54]])), times, cumsum(times)))
+rts <- as.data.frame(cbind(c(1:length(retweets)), times, cumsum(times)))
 colnames(rts) <- c("Count", "ms", "TotalTime")
 
 
